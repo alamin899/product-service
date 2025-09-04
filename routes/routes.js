e0a27d9beb3f02c23bd65b66cc70dc4e6756cd5b
alamin899 <alaminhossen899@gmail.com>
2025-09-04 11:17:27 +0600

    two method cpu and ram heavy test

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,21 @@ import { productValidator } from '../validators/storeProductValidator.js';
 
 const router = express.Router();
 
+router.get('/cpu-heavy', (req, res) => {
+  const calculateFibonacci = (n) => {
+    if (n <= 1) return n;
+    return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+  };
+
+  const result = calculateFibonacci(40); // Adjust the number for higher/lower CPU usage
+  res.json({ message: 'CPU-heavy task completed', result });
+});
+
+// RAM-heavy route: Simulates high memory usage by creating a large array
+router.get('/ram-heavy', (req, res) => {
+  const largeArray = Array(1e7).fill('RAM-heavy test'); // Creates an array with 10 million elements
+  res.json({ message: 'RAM-heavy task completed', size: largeArray.length });
+});
 router.get('/', productList);
 router.post('/', verifyToken,productValidator,validateMiddleware, storeProduct); // 👈 protected
 
